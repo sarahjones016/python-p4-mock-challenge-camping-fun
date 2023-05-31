43db8ab95d48fda70f4f026d914463ef93504632
Sarah Jones <sarahrjones8@gmail.com>
2023-05-31 11:25:24 -0400

    complete mock

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Activity, Camper
+from models import db, Activity, Camper, Signup
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
@@ -19,9 +19,146 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
 @app.route('/')
 def home():
     return ''
 
+class Campers(Resource):
+    def get(self):
+        campers_dict = [campers.to_dict(only = ("id", "name", "age")) for campers in Camper.query.all()]
+
+        response = make_response(
+            campers_dict, 
+            200
+        )
+        
+        return response 
+    
+    def post(self):
+
+        try:
+            new_camper = Camper(
+                name=request.json['name'],
+                age=request.json['age']
+            )
+
+            db.session.add(new_camper)
+            db.session.commit()
+
+            new_camper_dict = new_camper.to_dict(only = ("name", "age", "id"))
+
+            response = make_response(
+                new_camper_dict,
+                201
+            )
+
+            return response
+        
+        except:
+            return {"error": "400: Validation error"}, 400
+    
+api.add_resource(Campers, '/campers')
+
+class CampersById(Resource):
+    def get(self, id):
+        camper = Camper.query.filter_by(id = id).first()
+
+        if camper: 
+            camper_dict = camper.to_dict()
+
+            response = make_response(
+                camper_dict,
+                200
+            )
+
+            return response
+        
+        return {"error": "404: Camper not found"}, 404
+    
+api.add_resource(CampersById, '/campers/<int:id>')
+
+class Activities(Resource):
+    def get(self):
+        activities_dict = [activities.to_dict(only = ("id", "name", "difficulty")) for activities in Activity.query.all()]
+
+        response = make_response(
+            activities_dict, 
+            200
+        )
+        
+        return response 
+    
+api.add_resource(Activities, '/activities')
+
+class AcitivitiesById(Resource):
+    def get(self, id):
+        activity = Activity.query.filter_by(id = id).first()
+        
+        activity_dict = activity.to_dict(only = ("id", "name", "difficulty"))
+
+        response = make_response(
+            activity_dict,
+            200
+        )
+
+        return response
+    
+    def delete(self, id):
+        activity = Activity.query.filter_by(id = id).first()
+        if activity:
+
+            db.session.delete(activity)
+            db.session.commit()
+
+            response = make_response(
+                "",
+                204
+            )
+
+            return response
+        
+        return {"error": "404: Activity not found"}, 404
+
+    
+api.add_resource(AcitivitiesById, '/activities/<int:id>')
+
+class Signups(Resource):
+    def get(self):
+        signups_dict = [signups.to_dict() for signups in Signup.query.all()]
+
+        response = make_response(
+            signups_dict, 
+            200
+        )
+        
+        return response 
+    
+    def post(self):
+        try:
+            new_signup = Signup(
+                time=request.json['time'],
+                camper_id=request.json['camper_id'],
+                activity_id=request.json['activity_id']
+            )
+
+            db.session.add(new_signup)
+            db.session.commit()
+
+            new_signup_dict = new_signup.to_dict()
+
+            response = make_response(
+                new_signup_dict,
+                201
+            )
+
+            return response
+        except:
+            return {"error": "400: Validation error"}, 400
+
+api.add_resource(Signups, '/signups')
+
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -22,6 +22,12 @@ class Activity(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    signups = db.relationship("Signup", backref='activity')
+
+    serialize_rules = ('-signups.activity',)
 
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -32,6 +38,24 @@ class Camper(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    signups = db.relationship("Signup", backref='camper')
+
+    serialize_rules = ('-signups.camper',)
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Camper must have a name")
+        return name
+    
+    @validates('age')
+    def validate_age(self, key, age):
+        if not 8 <= age <= 18:
+            raise ValueError("Camper's age must be between 8 and 18")
+        return age
 
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
@@ -40,9 +64,23 @@ class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
 
     id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+
+    serialize_rules = ('-camper.signups', '-activity.signups',)
 
     def __repr__(self):
         return f'<Signup {self.id}>'
+    
+    @validates('time')
+    def validate_time(self, key, time):
+        if not 0 <= time <= 23:
+            raise ValueError("Time must be between 0 and 23")
+        return time
 
 
 # add any models you may need. 
